71afa3778601537a227f6020f9b37011adc950f2
Danny Zhan <danny.zhan@hotmail.com>
2019-05-22 23:21:46 +0800

    update

--- assets/themes/sext-v/js/staticman.js
+++ assets/themes/sext-v/js/staticman.js
@@ -14,7 +14,7 @@
       data: $(this).serialize(),
       contentType: 'application/x-www-form-urlencoded',
       success: function (data) {
-        showModal('Comment submitted', 'Thanks! Refresh your browser in a minute to see your comment.');
+        showModal('成功', '恭喜，点评成功! 请在1分钟后刷新浏览器以查看你的点评.');
 //        showModal('Comment submitted', 'Thanks! Your comment is <a href="https://github.com/willymcallister/willymcallister.github.io/pulls">pending</a>. It will appear when approved.');
           $('form__spinner').removeClass('form--loading');
       },
